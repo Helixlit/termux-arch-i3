afaf145c32189e1d0dc77ca3d1b5e573f57bd027
BuildTools <unconfigured@null.spigotmc.org>
2024-09-14 18:50:24 +0200

    adf

--- termux-arch-i3-setup.sh
+++ termux-arch-i3-setup.sh
@@ -8,7 +8,7 @@ useradd -m $user_name
 passwd $user_name
 
 # install needed packages
-pacman --noconfirm -S sudo glibc i3 kitty dmenu firefox
+pacman --noconfirm -S sudo glibc i3 dmenu firefox alacritty
 # install all dependencies
 pacman --noconfirm -Syu
 
