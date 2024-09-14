0a65677402942974e17eac139cec5b79d17822db
BuildTools <unconfigured@null.spigotmc.org>
2024-09-14 18:04:36 +0200

    added firefox

--- termux-arch-i3-setup.sh
+++ termux-arch-i3-setup.sh
@@ -8,7 +8,7 @@ useradd -m $user_name
 passwd $user_name
 
 # install needed packages
-pacman --noconfirm -S sudo glibc i3 kitty dmenu
+pacman --noconfirm -S sudo glibc i3 kitty dmenu firefox
 # install all dependencies
 pacman --noconfirm -Syu
 
